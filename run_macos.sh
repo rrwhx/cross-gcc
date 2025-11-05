1fc0e75f8d0d5a9caa84417e4ed55c6a42d4d79a
Xinyu Li <1939199010@qq.com>
2025-11-06 00:17:30 +0800

    better macos support
    Signed-off-by: Xinyu Li <1939199010@qq.com>

--- run_macos.sh
+++ run_macos.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+
+# brew install bash gnu-sed gawk make bison rsync grep coreutils gcc
+
 ulimit -n 16384
+DARWIN_VERSION=$(uname -r | cut -d. -f1)
 env \
-    CXX=aarch64-apple-darwin24-g++-15 \
-    CC=aarch64-apple-darwin24-gcc-15 \
+    CXX=aarch64-apple-darwin${DARWIN_VERSION}-g++-15 \
+    CC=aarch64-apple-darwin${DARWIN_VERSION}-gcc-15 \
     MAKE=/opt/homebrew/opt/make/libexec/gnubin/make \
     PATH="/opt/homebrew/bin:/opt/homebrew/opt/coreutils/libexec/gnubin:/opt/homebrew/opt/grep/libexec/gnubin:/opt/homebrew/opt/findutils/libexec/gnubin:/opt/homebrew/opt/bison/bin:/opt/homebrew/opt/make/libexec/gnubin:/opt/homebrew/opt/gawk/libexec/gnubin:/opt/homebrew/opt/gnu-sed/libexec/gnubin:$PATH" \
     "$@"
